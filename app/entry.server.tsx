6875ffa256c5046e3ccb0e764b2664bc88c36d04
Renato Stretti <rstretti@gmail.com>
2023-03-07 01:13:36 +1100

    added netlify integration

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,11 +3,29 @@ import type { EntryContext } from "@remix-run/node";
 import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
+import { renderToPipeableStream, renderToString } from "react-dom/server";
 
 const ABORT_DELAY = 5000;
 
 export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  const markup = renderToString(
+    <RemixServer context={remixContext} url={request.url} />
+  );
+
+  responseHeaders.set("Content-Type", "text/html");
+
+  return new Response("<!DOCTYPE html>" + markup, {
+    headers: responseHeaders,
+    status: responseStatusCode,
+  });
+}
+
+function old_handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
